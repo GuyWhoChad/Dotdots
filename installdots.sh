752f89678b4d68ff2751d40298f7fb65c9760c27
GuyWhoChad <105840356+GuyWhoChad@users.noreply.github.com>
2024-02-06 13:55:39 +0530

    Cleaning up and removing archinstall option.

--- installdots.sh
+++ installdots.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
-archsetup() {
-	echo "Welcome to archlinux setup!"
-	pacman -Sy
-	echo "what partitioning tool would you like to use"
-	read partool
-	echo "Please specify disk."
-	read disk
-	if [[ $partool = "fdisk" ]]; then
-		fdisk $disk
-	elif [[ $partool = "cfdisk" ]]; then
-		cfdisk $disk
-	else
-		echo "Not a valid partitioning tool."
-		exit 1
-	fi
-	echo "Please specify mount points"
-	echo "/ parition?"
-	read $paroot
-	echo "Are you using a EFI SYSTEM PARITION? (y/n)"
-	read $efi
-	if [[ $efi = "y" ]]; then
-		echo "Specify EFI Partition"
-		read $efimnt
-	else
-		echo "Ok"
-	fi
+# arch setup code is deprecated as arch has an official script now!
+#archsetup() {
+#	echo "Welcome to archlinux setup!"
+#	pacman -Sy
+#	echo "what partitioning tool would you like to use"
+#	read partool
+#	echo "Please specify disk."
+#	read disk
+#	if [[ $partool = "fdisk" ]]; then
+#		fdisk $disk
+#	elif [[ $partool = "cfdisk" ]]; then
+#		cfdisk $disk
+#	else
+#		echo "Not a valid partitioning tool."
+#		exit 1
+#	fi
+#	echo "Please specify mount points"
+#	echo "/ parition?"
+#	read $paroot
+#	echo "Are you using a EFI SYSTEM PARITION? (y/n)"
+#	read $efi
+#	if [[ $efi = "y" ]]; then
+#		echo "Specify EFI Partition"
+#		read $efimnt
+#	else
+#		echo "Ok"
+#	fi
 
-	if [[ $efi = "y" ]]; then
-		mkfs.fat -F 32 $efimnt
-	else 
-		echo "Skipping efi creation"
-	fi
-	mkfs.ext4 $paroot
-	mount $paroot /mnt
-	if [[ $efi = "y" ]]; then
-		mount --mkdir $efimnt /mnt/boot
-	else
-		echo "No efi, skipping efi mount"
-	fi
+#	if [[ $efi = "y" ]]; then
+#		mkfs.fat -F 32 $efimnt
+#	else 
+#		echo "Skipping efi creation"
+#	fi
+#	mkfs.ext4 $paroot
+#	mount $paroot /mnt
+#	if [[ $efi = "y" ]]; then
+#		mount --mkdir $efimnt /mnt/boot
+#	else
+#		echo "No efi, skipping efi mount"
+#	fi
 
-	pacstrap -K /mnt base linux linux-firmware sudo nano vim grub
+#	pacstrap -K /mnt base linux linux-firmware sudo nano vim grub
 
-	genfstab -U /mnt >> /mnt/etc/fstab
-	arch-chroot /mnt ./part2.sh
+#	genfstab -U /mnt >> /mnt/etc/fstab
+#	arch-chroot /mnt ./part2.sh
 
 
-}
-echo "Would you like to install arch linux"
-read ans
+#}
+#echo "Would you like to install arch linux"
+#read ans
 
-if [[ $ans = "y" ]]; then
-	archsetup
-else
-	echo "nah"
-fi
-exit 1 
+#if [[ $ans = "y" ]]; then
+#	archsetup
+#else
+#	echo "nah"
+#fi
+#exit 1 
 echo "Welcome to my install script for my dotfiles, run at your own risk, no helps or issues will be answered!"
 sudo pacman -S $(cat configs/scripts/todeps.txt)
 sudo systemctl enable NetworkManager
